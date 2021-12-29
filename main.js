3c477cd41a6525ff58f6798cd01522fa8cbda030
Pratik Neupane <59110260+pratikneupane@users.noreply.github.com>
2021-12-29 19:48:54 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,42 @@
-import './style.css'
-
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
+const navbar = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 62) {
+    navbar.classList.add("fixed");
+  } else {
+    navbar.classList.remove("fixed");
+  }
+});
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    links.classList.remove("show-links");
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    let position;
+    if (navbar.classList.contains("fixed")) {
+      position = element.offsetTop - 62;
+    } else {
+      position = element.offsetTop - 124;
+    }
+    if (window.innerWidth < 992) {
+      if (navbar.classList.contains("fixed")) {
+        position = element.offsetTop - 62;
+      } else {
+        position = element.offsetTop - 332 - 62;
+      }
+    }
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: "smooth",
+    });
+  });
+});
